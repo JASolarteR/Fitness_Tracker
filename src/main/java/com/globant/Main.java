9e9cd5e959de7d39eab75bf895e49842db884fd1
JASolarteR <j.solarte@globant.com>
2025-04-01 16:04:34 -0500

    feat: Add password validation to User registration

--- src/main/java/com/globant/Main.java
+++ src/main/java/com/globant/Main.java
@@ -6,12 +6,22 @@ import com.globant.users.UserRepository;
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         UserRepository userRepo = new UserRepository();
-        userRepo.registerUser(new RegularUser("Juan", "Solarte", "j.solarte@globant.com", "123456789", false));
-        System.out.println(userRepo.getUsers());
-        userRepo.registerUser(new RegularUser("Juan", "Solarte", "j.solarte@globant.com", "123456789", false));
-        userRepo.loginUser("j.solarte@globant.com", "12345678");
+        System.out.println("Enter your first name: ");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter your last name: ");
+        String lastName = scanner.nextLine();
+        System.out.println("Enter your email: ");
+        String email = scanner.nextLine();
+        System.out.println("Enter your password: ");
+        String password = scanner.nextLine();
+        boolean userRegisteredSuccessfully = userRepo.registerUser(firstName, lastName, email, password);
+        if (userRegisteredSuccessfully) {
+            System.out.println(userRepo.getUsers());
+        }
+
     }
 }
--- src/main/java/com/globant/users/UserRepository.java
+++ src/main/java/com/globant/users/UserRepository.java
@@ -14,27 +14,44 @@ public class UserRepository {
         return new HashMap<>(users);
     }
 
-    public boolean registerUser(User user){
-        if (users.containsKey(user.getEmail())){
+    public boolean registerUser(String firstName, String lastName, String email, String password) {
+        if (users.containsKey(email)) {
             System.out.println("Email already exists!");
             return false;
         }
-        users.put(user.getEmail(), user);
+        if (!validatePassword(password)) {
+            return false;
+        }
+        User user = new RegularUser(firstName, lastName, email, password, false);
+        users.put(email, user);
         System.out.println("User registered successfully!");
-
         return true;
     }
 
-    public boolean loginUser(String email, String password){
-        if (!users.containsKey(email)){
+    public boolean loginUser(String email, String password) {
+        if (!users.containsKey(email)) {
             System.out.println("Email not found!");
             return false;
         }
-        if (!users.get(email).getPassword().equals(password)){
+        if (!users.get(email).getPassword().equals(password)) {
             System.out.println("Password incorrect, try again!");
             return false;
         }
         System.out.println("Login was successful!");
+        return true;
+    }
+
+    public boolean validatePassword(String password) {
+        String MAX_LENGTH = "8";
+        String ONE_DIGIT = "(?=.*[0-9])";
+        String LOWER_CASE = "(?=.*[a-z])";
+        String UPPER_CASE = "(?=.*[A-Z])";
+        String regExp = "^"+ONE_DIGIT+LOWER_CASE+UPPER_CASE+".{8,20}$";
+        if (password.length() < 8 || !password.matches(regExp)) {
+            System.out.println("Passwords must be at least 8 characters long and should include " +
+                    "at least one uppercase letter, one lowercase letter, one digit");
+            return false;
+        }
         return true;
     }
 
